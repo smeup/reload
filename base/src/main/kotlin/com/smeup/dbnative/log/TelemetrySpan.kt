712e42aeac1bd6bc07b20ab585007c5cc40eeabf
marcbarnaba <marco.barnaba@apuliasoft.com>
2024-09-06 17:25:45 +0200

    fix: update ot configuration

--- base/src/main/kotlin/com/smeup/dbnative/log/TelemetrySpan.kt
+++ base/src/main/kotlin/com/smeup/dbnative/log/TelemetrySpan.kt
@@ -4,10 +4,6 @@ import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.trace.Span
 import io.opentelemetry.context.Context
 import io.opentelemetry.context.Scope
-import io.opentelemetry.sdk.OpenTelemetrySdk
-import io.opentelemetry.sdk.trace.SdkTracerProvider
-import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor
-import io.opentelemetry.exporter.logging.LoggingSpanExporter
 
 class TelemetrySpan(spanName: String) {
 
@@ -15,11 +11,9 @@ class TelemetrySpan(spanName: String) {
     private var scope: Scope? = null
 
     init {
+        val serviceName = System.getenv("OTEL_SERVICE_NAME") ?: "defaultServiceName"
 
-        val ot = initializeOpenTelemetry()
-
-        val tracerServiceName = System.getenv("OTEL_SERVICE_NAME") ?: "defaultReloadTracer"
-        val tracer = ot?.tracerProvider?.get(tracerServiceName) ?: GlobalOpenTelemetry.getTracer(tracerServiceName)
+        val tracer = GlobalOpenTelemetry.getTracer(serviceName)
 
         span = tracer.spanBuilder(spanName)
             .setParent(Context.current())
@@ -28,22 +22,6 @@ class TelemetrySpan(spanName: String) {
         scope = span?.makeCurrent()
     }
 
-    private fun initializeOpenTelemetry(): OpenTelemetrySdk? {
-
-        val isConsoleLoggingEnabled = System.getenv("ENABLE_CONSOLE_LOGGING")?.toBoolean() ?: true
-
-        if (isConsoleLoggingEnabled) {
-            val loggingExporter = CustomSpanExporter(LoggingSpanExporter.create())
-            val tracerProvider = SdkTracerProvider.builder()
-                .addSpanProcessor(SimpleSpanProcessor.create(loggingExporter))
-                .build()
-
-            return OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).build()
-        }
-
-
-        return null
-    }
 
     fun endSpan() {
         span?.end()
